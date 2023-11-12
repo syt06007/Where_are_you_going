bec43564cdb2f88e96708c3cf32683e69a3cf562
Kim HyeongSik <syt06007@naver.com>
2023-11-12 18:21:26 +0900

    pose net

--- sub_map_khs.py
+++ sub_map_khs.py
@@ -10,6 +10,8 @@ import cv2
 from torchvision import transforms
 from posenet.posenet_resnet50_ import PoseNet
 from yolo_qr import QRdecoder
+from PIL import Image
+import sys
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('Machine is',device)
@@ -37,46 +39,68 @@ class Pathplanner():
         self.my_y = None
 
         # Path planning
-        self.Kp_att = 0.5
+        self.Kp_att = 1
         self.Kp_rel = 30
         self.obstacle_bound = 50
+        self.path = None
+
+        # junction location on map
+        self.map_junction = [323, 180]
 
         # QR decoder
         self.qrdecoder = QRdecoder()
         
-
+    def convert_pose_to_map(self, x, y):
+        x_map = int( (x-self.origin_x) / self.resolution)
+        y_map = int( (y-self.origin_y) / self.resolution)
+        return [x_map, y_map]
+    
+    def convert_map_to_pose(self, x, y):
+        x_pose = x*self.resolution + self.origin_x
+        y_pose = y*self.resolution + self.origin_y
+        return [x_pose, y_pose]
+    
     def set_goal(self):
         self.destination = input("Where are you go? :")
-        self.goal_x = 6.5
-        self.goal_y = -3
-        # if self.destination == "toilet":
-        #     self.goal_x = 6.5
-        #     self.goal_y = -3
+        
+        if self.destination == "toilet":
+            self.goal_x = 6.5
+            self.goal_y = -3
+            print("toilet : ({0}, {1})".format(self.goal_x, self.goal_y))
         # else:
         #     print("none")
             
+    def visualizer(self):
+        pyplot.imshow(self.map)
+        pyplot.show()
+        pyplot.pause(0.001)
+    
+    def draw_current_pose2map(self, pose):
+        self.map[pose[1], pose[0]] = 255
         
+
     def planning(self):
-
+        
         print("target point : ", self.goal_x, self.goal_y)
-
-        self.map_goal_xy = [int((self.goal_x - self.origin_x) / self.resolution), int((self.goal_y - self.origin_y) / self.resolution)] 
-        self.map[self.map_goal_xy[1], self.map_goal_xy[0]] = 200
+        self.visualizer()
+        self.map_goal_xy = self.convert_pose_to_map(self.goal_x, self.goal_y)
+        self.map[self.map_goal_xy[1], self.map_goal_xy[0]] = 50
 
         self.my_x, self.my_y = 2.1, -0.9
 
 
         obs_idx = np.array(np.where(self.map == 100)).T
         
-        path = self.Artificial_potential_field(self.my_x, self.my_y, obs_idx)
+        self.path = self.Artificial_potential_field(self.my_x, self.my_y, obs_idx)
       
-        for i in range(len(path[0])):
-            x = path[0][i]
-            y = path[1][i]
-            self.map[y, x] = 255  
+        for i in range(len(self.path[0])):
+            x = self.path[0][i]
+            y = self.path[1][i]
+            self.map[y, x] = 100
+        
+        self.visualizer()
 
-        pyplot.imshow(self.map)
-        pyplot.show()
+        
 
     def att_force(self, x, y):
         e_x, e_y = self.map_goal_xy[0]-x, self.map_goal_xy[1]-y
@@ -91,7 +115,7 @@ class Pathplanner():
     def rep_force(self, x, y, obs):
 
         rep_x, rep_y = 0, 0
-        print(max(obs[:,0]), max(obs[:,1]))
+        #print(max(obs[:,0]), max(obs[:,1]))
         for obs_xy in np.ndindex(obs.shape[0]):
             #print("obs , ",obs[obs_xy][0], obs[obs_xy][1], x, y)
             obs_dis_x, obs_dis_y = obs[obs_xy][1]-x, obs[obs_xy][0]-y
@@ -107,9 +131,8 @@ class Pathplanner():
     
 
     def Artificial_potential_field(self, start_x, start_y, obs):
-
-        x, y = (start_x - self.origin_x)/self.resolution, (start_y-self.origin_y)/self.resolution
-
+        max_error = None
+        [x, y] = self.convert_pose_to_map(start_x, start_y)
         trace_x = []
         trace_y = []
 
@@ -121,20 +144,25 @@ class Pathplanner():
             
             rep_x, rep_y = self.rep_force(x, y, obs)
 
-            print(rep_x, rep_y)
+            #print(rep_x, rep_y)
             pot_x = att_x + rep_x
             pot_y = att_y + rep_y
 
             x = x + pot_x
             y = y + pot_y
-            print("cur x",x,"cur y",y, "target:", self.map_goal_xy[0], self.map_goal_xy[1])
+            #print("cur x",x,"cur y",y, "target:", self.map_goal_xy[0], self.map_goal_xy[1])
             trace_x.append(int(x))
             trace_y.append(int(y))
 
             error = np.linalg.norm([self.map_goal_xy[0]-x, self.map_goal_xy[1]-y])
-            print("err :", error)
+            if max_error is None:
+                max_error = error
+            progress_status = ( np.abs(max_error - error) / max_error ) * 100
+            #print("progress status : {:.2f}%".format(progress_status)) 
+            sys.stdout.write("\r progress status : [%-20s] %d%%" % ('='*int(progress_status//5), progress_status))
+            sys.stdout.flush()
             if error < 1:
-                print("Path planning end")
+                print("\nPath planning end")
                 return [trace_x, trace_y]
                 break
 
@@ -172,12 +200,12 @@ class Pathplanner():
         if cap.isOpened():
             while True:
                 ret, frame = cap.read()
-                print("r")
+                #print("r")
                 if ret is False:
                     continue
                 else:
                     cv2.imshow('fre', frame)
-                    cv2.waitKey(10)
+                    cv2.waitKey(1)
 
                     # frame을 수정해서 qr을 인식하도록 함
                     blob = cv2.dnn.blobFromImage(frame, 1/255, (416, 416), swapRB=True, crop=False)
@@ -211,43 +239,77 @@ class Pathplanner():
             cv2.destroyAllWindows()
         else:
             print("Unable to open camera")
-    
+
+    def navigate(self, map_pose):
+        dist_to_junction = np.linalg.norm(np.array(self.map_junction) - np.array(map_pose))
+        print(dist_to_junction)
+        if dist_to_junction < 10:
+            dx, dy = map_pose[0] - self.map_goal_xy[1], map_pose[1] - self.map_goal_xy[0]
+            angle = np.arctan2(dy, dx)
+            print(dx, dy, angle)
+            if angle > np.pi /4:
+                print("turn left")
+            elif angle < np.pi / 4:
+                print("turn right")
+        else:
+            print("Go straight")
+
+                
+
+
+
     def localize(self):
         print("localization started")
-        # cap = cv2.VideoCapture(self.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+        cap = cv2.VideoCapture(self.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
         
         mean=[0.49, 0.486, 0.482] 
         std=[0.197, 0.189, 0.187]   
-        tf = transforms.Compose([transforms.ToTensor(), transforms.Resize((224, 224)), transforms.Normalize(mean, std)])
-
+        #tf = transforms.Compose([transforms.ToTensor(), transforms.Resize((224, 224)), transforms.Normalize(mean, std)])
+        tf = transforms.Compose([transforms.Resize((224,224)), transforms.ToTensor(), transforms.Normalize(mean, std)])
         # Test cap
-        cap = cv2.VideoCapture(0)
+        #cap = cv2.VideoCapture(0)
         # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 224)
         # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 224)
 
         # model init
         model = PoseNet()
         model.to(device)
-        model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+        model.load_state_dict(torch.load(model_path))
         model.eval()
 
         # localization loop
         while cap.isOpened():
-            ret_val, frame = cap.read()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = tf(frame)
+            ret_val, cv_frame = cap.read()
+            cv_frame = cv2.cvtColor(cv_frame, cv2.COLOR_BGR2RGB)
+            # cv2.imshow('cv', cv_frame)
+            # cv2.waitKey(1)
+            frame = Image.fromarray(cv_frame)
+            frame = tf(frame).to(device)
+            
             # posenet inference
 
             pose = model(frame.unsqueeze(0))
-            print(f'-------------IM Pose-------------\n{pose}')
+            pose = pose.cpu().detach().numpy()
+            self.my_x, self.my_y = pose[0][0][0], pose[0][0][1]
+
+            print("current location : {:.2f}, {:.2f}".format(self.my_x, self.my_y))
+
+
+            pose_map = self.convert_pose_to_map(self.my_x, self.my_y)
+            print("pose in map : {0}, {1}".format(pose_map[0], pose_map[1]))
+            self.navigate(pose_map)
+
+            self.draw_current_pose2map(pose_map)
+            self.visualizer()
 
 
 if __name__ == '__main__':
 
     planner = Pathplanner()
-    # planner.set_goal()
-    planner.initialize()
-    # planner.planning()
+    pyplot.ion()
+    planner.set_goal()
+    #planner.initialize()
+    planner.planning()
     planner.localize()
 
 
